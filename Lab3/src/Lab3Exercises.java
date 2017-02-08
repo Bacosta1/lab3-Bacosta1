8572e69160a9c64663ddcea62b6e28a993ea80c9
4015d1 <4015d1@papaya.ece.uprm.edu>
2017-02-08 14:05:06 -0400

    Changed name and size

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Bryan Acosta!");
+		JFrame myFrame = new JFrame("Bryan!");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
 		myFrame.setSize(200, 200);
